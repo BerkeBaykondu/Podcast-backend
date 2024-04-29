f85ac0fdac89e8cfe158339007ed4decf4095017
BerkeBaykondu <berke.baykondu@boun.edu.tr>
2024-04-29 21:33:57 +0300

    Change cors option

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true })
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '', // Allow only this origin
+      credentials: true, // Allow credentials like cookies
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow these HTTP methods
+    },
+  })
   await app.listen(3000)
 }
 bootstrap()
