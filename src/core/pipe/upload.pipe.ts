18c6a5f9ee14b2454da8ad188c4fb708a8c30d86
BerkeBaykondu <berke.baykondu@boun.edu.tr>
2024-05-02 23:08:15 +0300

    One exact audio and one exact webp uploaded

---
+++ src/core/pipe/upload.pipe.ts
@@ -0,0 +1,26 @@
+// file-type.pipe.ts
+import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common'
+import { FileUpload } from '../model/http-upload-file-interface'
+
+@Injectable()
+export class FileTypePipe implements PipeTransform<any> {
+  async transform(value: any): Promise<any> {
+    console.log(value)
+
+    if (!value || value.length !== 2) {
+      throw new BadRequestException('Exactly two files are required')
+    }
+
+    const webpFile = value.find((file: any) => file.mimetype === 'image/webp')
+    const audioFile = value.find((file: any) => file.mimetype.startsWith('audio/'))
+
+    console.log(webpFile)
+
+    if (!webpFile || !audioFile) {
+      throw new BadRequestException('One webp and one audio file are required')
+    }
+    console.log(webpFile)
+
+    return [webpFile, audioFile]
+  }
+}
